ca5e9960b9e89e6b016102d362d21ef580b28d17
Musabii Mambetov <musabi@live.com>
2022-07-18 15:01:00 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    context:path.join(__dirname,'src'),
+    mode:'development',
+    entry:['@babel/polyfill','./index.js'],
+    output:{
+        filename:'bundle.js',
+        path:path.join(__dirname,'dist')
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./index.html'
+        }),new MiniCssExtractPlugin({
+            filename:'[hash].[contenthash].css'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css/,
+                use:[MiniCssExtractPlugin.loader,"css-loader"]
+            },
+            {
+                test:/\.js/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
